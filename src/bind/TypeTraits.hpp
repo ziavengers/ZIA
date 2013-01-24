7ba2b2be3c53c18676d7d9e91f3393997dcf32cd
rivot_c <corentin.rivot@epitech.eu>
2013-01-24 21:33:21 +0100

    bind : TypeTraits de base

---
+++ src/bind/TypeTraits.hpp
@@ -0,0 +1,22 @@
+/*
+** TypeTraits.hpp for  in /home/nuts/local/ZIA/src/bind
+** 
+** Made by 
+** Login   <nuts@epitech.net>
+** 
+** Started on  Thu Jan 24 21:32:36 2013 
+** Last update Thu Jan 24 21:32:36 2013 
+*/
+
+#ifndef		TYPETRAITS_HPP_
+# define	TYPETRAITS_HPP_
+
+template < >
+
+class TypeTraits
+{
+    public:
+	TypeTrais() { }
+};
+
+#endif		/* !TYPETRAITS_HPP_*/
