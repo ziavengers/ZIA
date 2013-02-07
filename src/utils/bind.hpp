14658e6869e7d15d7c852ff642203579c8c19073
entwanne <antoine.rozo@gmail.com>
2013-02-07 19:56:26 +0100

    Déplacement de bind.hpp et stock_callback.hpp dans le dossier parent Le répertoire bind n'a à être utilisé que pour la génération

---
+++ src/utils/bind.hpp
@@ -0,0 +1,11 @@
+#ifndef BIND_HPP_
+#define BIND_HPP_
+
+#include "bind/value.hpp"
+#include "bind/build/storage.hpp"
+#include "bind/build/typelist.hpp"
+#include "bind/build/traits.hh"
+#include "bind/caller.hpp"
+#include "bind/build/bind.hpp"
+
+#endif
