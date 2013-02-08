3222134623f234a90e7e2ad3ba93910b4d7c438c
entwanne <antoine.rozo@gmail.com>
2013-02-08 10:55:31 +0100

    Réflexions autour de la classe Object Et création d'un fichier de test dans src/core/

--- inc/core/Object.hpp
+++ inc/core/Object.hpp
@@ -8,38 +8,40 @@ namespace zia
 
 #include <map>
 #include <string>
-#include "src/utils/bind.hpp"
+#include "../utils/bind.hpp"
 
     class Object
     {
     public:
-      Signal() { }
-      ~Signal() { }
+      Object() {}
+      ~Object() {}
 
       template < typename T >
+      void connect(const std::string&, T)
+      {}
 
-      void	connect(const std::string& s, T *func)
-      {
-	_calling[s] = new StockCallback(bind(func));
-      }
+      // void	connect(const std::string& s, T *func)
+      // {
+      // 	_calling[s] = new StockCallback(bind(func));
+      // }
 
-      template < typename T >
+      // template < typename T >
 
-      void	connect(const std::string& s, int (T::*func)(), T* obj)
-      {
-	_calling[s] = new StockCallback(bind< int >(func, *obj));
-      }  
+      // void	connect(const std::string& s, int (T::*func)(), T* obj)
+      // {
+      // 	_calling[s] = new StockCallback(bind< int >(func, *obj));
+      // }  
   
-      void	emit(const std::string& s)
-      {
-	(*_calling[s])();
-      }
+      // void	emit(const std::string& s)
+      // {
+      // 	(*_calling[s])();
+      // }
+
+      void emit(const std::string&)
+      {}
 
     protected:
       std::map< std::string, StockCallback* >	_calling;
-
-
-    private:
     };
 
   }
