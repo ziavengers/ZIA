07564ba719de762b52c5dc661e52b732420dd554
entwanne <antoine.rozo@gmail.com>
2013-02-10 13:06:54 +0100

    APIException devient Exception

--- inc/utils/Exception.hpp
+++ inc/utils/Exception.hpp
@@ -1,5 +1,5 @@
-#ifndef APIEXCEPTION_HPP_
-#define APIEXCEPTION_HPP_
+#ifndef EXCEPTION_HPP_
+#define EXCEPTION_HPP_
 
 #include <exception>
 #include <string>
@@ -11,12 +11,12 @@ namespace zia
 namespace utils
 {
 
-  class APIException : public std::exception
+  class Exception : public std::exception
   {
   public:
-    APIException(const std::string& s) : _msg(s) { }
+    Exception(const std::string& s) : _msg(s) { }
     
-    virtual ~APIException() throw() { }
+    virtual ~Exception() throw() { }
 
     const char *	what() const throw() {
       LOG_ERROR(_msg);
