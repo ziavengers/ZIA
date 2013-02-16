e886683c3b03080feffa6c969ec2f351de6e5bd3
rivot_c <corentin.rivot@epitech.eu>
2013-02-16 22:40:19 +0100

    Ajout des namespaces dans les .cpp thread windows

--- src/thread/windows/AThread.cpp
+++ src/thread/windows/AThread.cpp
@@ -1,27 +1,35 @@
 #include "thread/AThread.hh"
 
-void	AThread::start()
+namespace zia
 {
-	// Normalement il ne faut pas caster en LPTHREAD... peut crash sous windows 64bits, à voir
-	if ((_thread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)&AThread::startRoutine, this, 0, NULL)) == NULL)
-	  ;//throw CoreException("AThread::start fail!");
-}
+  namespace thread
+  {
 
-void*	AThread::join()
-{
-	LPDWORD	ret;
+    void	AThread::start()
+    {
+      // Normalement il ne faut pas caster en LPTHREAD... peut crash sous windows 64bits, à voir
+      if ((_thread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)&AThread::startRoutine, this, 0, NULL)) == NULL)
+	;//throw CoreException("AThread::start fail!");
+    }
 
-	if (ResumeThread(_thread) == -1)
-	  ;//throw CoreException("AThread::join fail!");
-	if (GetExitCodeThread(_thread, ret) == 0)
-	  ;//throw CoreException("AThread::join fail!");
-	return ret;
-}
+    void*	AThread::join()
+    {
+      LPDWORD	ret;
 
-void*	AThread::startRoutine(LPVOID lpParam)
-{
-	AThread*	t;
+      if (ResumeThread(_thread) == -1)
+	;//throw CoreException("AThread::join fail!");
+      if (GetExitCodeThread(_thread, ret) == 0)
+	;//throw CoreException("AThread::join fail!");
+      return ret;
+    }
 
-	t = reinterpret_cast< AThread* >(lpParam);
-	return t->run();
+    void*	AThread::startRoutine(LPVOID lpParam)
+    {
+      AThread*	t;
+
+      t = reinterpret_cast< AThread* >(lpParam);
+      return t->run();
+    }
+
+  }
 }
--- src/thread/windows/Mutex.cpp
+++ src/thread/windows/Mutex.cpp
@@ -1,51 +1,59 @@
 #include "thread/Mutex.hh"
 
-
-Mutex::Mutex(void)
+namespace zia
 {
-  if ((_mutex = CreateMutex(NULL, FALSE, NULL)) == NULL)
-    ;//throw CoreException("Mutex::Constructor fail");
-}
+  namespace thread
+  {
 
-
-Mutex::~Mutex(void)
-{
-  if (CloseHandle(_mutex) == 0)
-    ;//throw CoreException("Mutex::Destructor fail");
-}
-
-void	Mutex::lock(void)
-{
-  switch (WaitForSingleObject(_mutex, INFINITE))
+    Mutex::Mutex(void)
     {
-    case WAIT_ABANDONED :
-      ;//throw CoreException("Mutex::lock fail, abandoned");
-      //case WAIT_OBJECT_0 :
-    case WAIT_TIMEOUT :
-      ;//throw CoreException("Mutex::lock fail, timeout");
-    case WAIT_FAILED :
-      ;//throw CoreException("Mutex::lock fail");
+      if ((_mutex = CreateMutex(NULL, FALSE, NULL)) == NULL)
+	;//throw CoreException("Mutex::Constructor fail");
     }
-}
 
-void	Mutex::unlock(void)
-{
-  if (ReleaseMutex(_mutex) == 0)
-    ;//throw CoreException("Mutex::unlock fail");
-}
 
-bool	Mutex::trylock(void)
-{
-  switch (WaitForSingleObject(_mutex, INFINITE))
+    Mutex::~Mutex(void)
     {
-    case WAIT_ABANDONED :
-      ;//throw CoreException("Mutex::lock fail, abandoned");
-    case WAIT_OBJECT_0 :
-      return true;
-    case WAIT_TIMEOUT :
-      ;//throw CoreException("Mutex::lock fail, timeout");
-    case WAIT_FAILED :
-      ;//throw CoreException("Mutex::lock fail");
+      if (CloseHandle(_mutex) == 0)
+	;//throw CoreException("Mutex::Destructor fail");
     }
-  return false;
+
+    void	Mutex::lock(void)
+    {
+      switch (WaitForSingleObject(_mutex, INFINITE))
+	{
+	case WAIT_ABANDONED :
+	  ;//throw CoreException("Mutex::lock fail, abandoned");
+	  //case WAIT_OBJECT_0 :
+	case WAIT_TIMEOUT :
+	  ;//throw CoreException("Mutex::lock fail, timeout");
+	case WAIT_FAILED :
+	  ;//throw CoreException("Mutex::lock fail");
+	}
+    }
+
+    void	Mutex::unlock(void)
+    {
+      if (ReleaseMutex(_mutex) == 0)
+	;//throw CoreException("Mutex::unlock fail");
+    }
+
+    bool	Mutex::trylock(void)
+    {
+      switch (WaitForSingleObject(_mutex, INFINITE))
+	{
+	case WAIT_ABANDONED :
+	  ;//throw CoreException("Mutex::lock fail, abandoned");
+	case WAIT_OBJECT_0 :
+	  return true;
+	case WAIT_TIMEOUT :
+	  ;//throw CoreException("Mutex::lock fail, timeout");
+	case WAIT_FAILED :
+	  ;//throw CoreException("Mutex::lock fail");
+	}
+      return false;
+    }
+
+  }
 }
+
