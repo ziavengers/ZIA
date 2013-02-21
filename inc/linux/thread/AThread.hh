03ad66ecccb1f77c1064cd7e094f11286af308d1
entwanne <antoine.rozo@gmail.com>
2013-02-21 15:18:48 +0100

    Suppression des méthodes peu utiles d'IThread et AThread

--- inc/linux/thread/AThread.hh
+++ inc/linux/thread/AThread.hh
@@ -15,10 +15,7 @@ namespace zia
       void		start();
       void*		join();
 
-      virtual void	init(void*) = 0;
       virtual void*	run() = 0;
-      virtual void	pause() = 0;
-      virtual void	resume() = 0;
 
     protected:
       pthread_t	_thread;
