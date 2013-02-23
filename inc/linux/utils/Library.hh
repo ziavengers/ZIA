58020600dc5cbf6c0873c987bedc2fc1227189a6
rivot_c <corentin.rivot@epitech.eu>
2013-02-23 15:51:41 +0100

    Library (for Unix) need -ldl - opening dynamic library - closing dynamic library - getting function for library

---
+++ inc/linux/utils/Library.hh
@@ -0,0 +1,51 @@
+#ifndef LIBRARY_HH_
+#define LIBRARY_HH_
+
+#include <map>
+#include <string>
+
+#include <dlfcn.h>
+
+#include "utils/Exception.hpp"
+#include "utils/NonCopyable.hh"
+
+namespace zia
+{
+  namespace utils
+  {
+
+    class Library : public NonCopyable
+    {
+    public:
+
+      explicit Library();
+      ~Library();
+
+      void	add(const std::string&, const std::string& = "") throw(Exception);
+      void	close(const std::string&);
+
+      template < typename T >
+      T	getFunction(const std::string& s, const std::string& funcName) const throw(Exception)
+      {
+	void	*tmp = _mlib[s];
+	if (tmp == 0)
+	  throw Exception(std::string("Library::get : Impossible de charger la fonction :") + funcName + std::string(" depuis ") + s);
+	
+	T	fptr = reinterpret_cast< T >(::dlsym(tmp, funcName.c_str()));
+	if (!fptr)
+	  throw Exception(std::string("Library::get : Impossible de charger la fonction :") + funcName + std::string(" depuis ") + s);
+	return fptr;	  
+      }
+
+      
+    private:
+      std::map< std::string, void* >	_mlib;
+    };
+
+
+  }
+}
+
+
+
+#endif
---
+++ src/utils/linux/Library.cpp
@@ -0,0 +1,45 @@
+#include "utils/Library.hh"
+
+namespace zia
+{
+  namespace utils
+  {
+
+    Library::Library()
+    {
+      // ...
+    }
+
+    Library::~Library()
+    {
+      std::map< std::string, void * >::iterator	it;
+
+      for (it = _mlib.begin(); it != _mlib.end(); ++it)
+	if ((*it).second)
+	  ::dlclose((*it).second);	
+    }
+
+    void	Library::add(const std::string& path, const std::string& name) throw(Exception)
+    {
+
+      void*	tmp = ::dlopen(path.c_str(), RTLD_LAZY);
+      if (tmp == 0)
+	throw Exception(std::string("Library:: Impossible de charger la librairie ") + path + std::string(::dlerror()));
+      
+      if (name.empty())
+	_mlib[path] = tmp;
+      else
+	_mlib[name] = tmp;
+    }
+
+    void	Library::close(const std::string& name)
+    {
+      void	*tmp = _mlib[name];
+
+      if (tmp)
+	::dlclose(tmp);
+      _mlib.erase(name);
+    }
+
+  }
+}
