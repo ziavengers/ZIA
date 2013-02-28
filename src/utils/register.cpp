42ee79d0898a2315635831afe99539df11ed7f0d
entwanne <antoine.rozo@gmail.com>
2013-02-28 13:58:50 +0100

    Ajout d'une classe pour enregistrer les types les plus communs

--- src/utils/register.cpp
+++ src/utils/register.cpp
@@ -9,3 +9,28 @@ namespace zia
 
   }
 }
+
+#include <string>
+
+class RegisterCommonTypes
+{
+public:
+  RegisterCommonTypes()
+  {
+    registerType(char);
+    registerType(int);
+    registerType(long);
+    registerType(float);
+    registerType(double);
+    registerType(unsigned char);
+    registerType(unsigned int);
+    registerType(unsigned long);
+    registerType(char*);
+    registerType(const char*);
+    registerType(int*);
+    registerType(std::string);
+    registerType(std::string&);
+    registerType(const std::string&);
+  }
+};
+static RegisterCommonTypes _;
