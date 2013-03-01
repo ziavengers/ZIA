6f993df2f06cfd150251d3fe4b365d16b9722247
thomas.ecuer <thomas.ecuer@epitech.eu>
2013-03-01 19:12:37 +0100

    clean du code

--- src/parsing_http/HttpMessage.cpp
+++ src/parsing_http/HttpMessage.cpp
@@ -5,22 +5,6 @@
 #include <fstream>
 #include <iostream>
 
-// // c'est un long namespace
-// std::string http::message::HttpMessage::toString(void)
-// {
-//   // return a string-representation of the request, usefull
-//   // for crafting request.
-//   // - header -
-//   // - message -
-
-//   // prevoir les problemes qui vont apparaitres a cause des encodages...
-
-
-
-
-//   return std::string("not implemented bastard");
-// }
-// =======
 namespace zia
 {
   namespace http
@@ -44,14 +28,3 @@ namespace zia
 }
 
 
-// void	testFromFile()
-// {
-//   std::ifstream file("./request_sample");
-
-//   std::cout << "start" << std::endl;
-//   std::string tmp;
-//   while (file >> tmp)
-//     {
-//       std::cout << tmp << std::endl ;
-//     }
-// }
