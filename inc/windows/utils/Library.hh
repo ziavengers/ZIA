519a83793ca14a258f6ccd3c67450074c2f91080
rivot_c <corentin.rivot@epitech.eu>
2013-03-06 11:01:34 +0100

    class Library to load Library for windows

---
+++ inc/windows/utils/Library.hh
@@ -0,0 +1,53 @@
+#ifndef LIBRARY_HH_
+#define LIBRARY_HH_
+
+#include <map>
+#include <string>
+
+#include <windows.h>
+
+#include "utils/Exception.hpp"
+#include "utils/NonCopyable.hh"
+
+
+namespace zia
+{
+  namespace utils
+  {
+
+
+    class Library : public NonCopyable
+    {
+    public:
+      explicit Library();
+
+      ~Library();
+
+
+      void	add(const std::string&, const std::string& = "") throw(Exception);
+      void	close(const std::string&);
+
+      template < typename T >
+      T	getFunction(const std::string& s, const std::string& funcName) throw(Exception)
+      {
+	HINSTANCE tmp = _mlib[s];
+	if (tmp == 0)
+	  throw Exception(std::string("Library::get : Impossible de charger la fonction :") + funcName + std::string(" depuis ") + s);
+	
+	T	fptr = reinterpret_cast< T >(::GetProcAddress(tmp, funcName.c_str()));
+	if (!fptr)
+	  throw Exception(std::string("Library::get : Impossible de charger la fonction :") + funcName + std::string(" depuis ") + s);
+	return fptr;
+      }      
+
+    private:
+      std::map< std::string, HINSTANCE >	_mlib;
+
+
+    };
+
+  }
+}
+
+
+#endif
---
+++ src/utils/windows/Library.cpp
@@ -0,0 +1,46 @@
+#include "utils/Library.hh"
+
+namespace zia
+{
+  namespace utils
+  {
+
+    Library::Library()
+    {
+      // ...
+    }
+
+    Library::~Library()
+    {
+      std::map< std::string, void * >::iterator	it;
+
+      for (it = _mlib.begin(); it != _mlib.end(); ++it)
+	if ((*it).second)
+	  ::FreeLibrary((*it).second);	
+    }
+
+    void	Library::add(const std::string& path, const std::string& name) throw(Exception)
+    {
+
+      void*	tmp = ::LoadLibrary(TEXT(path.c_str()));
+      if (tmp == 0)
+	throw Exception(std::string("Library:: Impossible de charger la librairie ") + path + std::string(::dlerror()));
+      
+      if (name.empty())
+	_mlib[path] = tmp;
+      else
+	_mlib[name] = tmp;
+    }
+
+    void	Library::close(const std::string& name)
+    {
+      HINSTANCE	tmp = _mlib[name];
+
+      if (tmp)
+	::FreeLibrary(tmp);
+      _mlib.erase(name);
+    }    
+
+
+  }
+}
