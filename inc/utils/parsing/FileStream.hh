614ac247c6fa628dd14403471ed2c989dc3f2a8b
entwanne <antoine.rozo@gmail.com>
2013-03-06 10:56:10 +0100

    FileStream

---
+++ inc/utils/parsing/FileStream.hh
@@ -0,0 +1,29 @@
+#ifndef FILEPRODUCTERSTREAM_HH_
+#define FILEPRODUCTERSTREAM_HH_
+
+#include <fstream>
+#include "IProducterStream.hh"
+
+namespace zia
+{
+  namespace utils
+  {
+    namespace parsing
+    {
+
+      class FileStream : public IProducterStream
+      {
+      public:
+	FileStream(const std::string&);
+	std::string nextString();
+      private:
+	std::ifstream _f;
+	static const size_t readSize = 512;
+	char _buff[readSize + 1];
+      };
+
+    }
+  }
+}
+
+#endif
