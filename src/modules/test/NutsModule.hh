544a810bb6bed233735e0a16fe8244a90e40ae0f
entwanne <antoine.rozo@gmail.com>
2013-03-08 16:54:25 +0100

    Suppression commentaires NutsModule

--- src/modules/test/NutsModule.hh
+++ src/modules/test/NutsModule.hh
@@ -10,10 +10,7 @@
 class	NutsModule : public zia::core::module::AModule
 {
 public:
-  // NutsModule(const std::string& s1, const std::string& s2) : AModule(s1, s2) {
-  //   _name = "NutsModule";
   NutsModule(const std::string& s1, const std::string& s2) : AModule(s1, s2, "NutsModule") {
-    // this->connect("test", zia::utils::bind(&NutsModule::test, *this));
     this->connect(s1, zia::utils::bind(&NutsModule::test, *this));
   }
 
@@ -23,10 +20,6 @@ public:
   {
     std::cout << "TEST called" << std::endl;
   }
-
-  // void	name(const std::string& name) { _name = name; }
-  // const std::string&        name() const { return _name; }
-  // void	version(int major, int minor) { _minor = minor; _major = major; };
 
 };
 
