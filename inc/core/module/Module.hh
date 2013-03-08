362ce72864109569633b8bfd6dd909c2fd5e0cc1
rivot_c <corentin.rivot@epitech.eu>
2013-03-08 16:35:15 +0100

    Plus de methode version et suppression de commentaire)

--- inc/core/module/Module.hh
+++ inc/core/module/Module.hh
@@ -23,8 +23,6 @@ namespace zia
 	virtual void	name(const std::string&) = 0;
 	virtual const std::string&	name() const = 0;
 
-	// virtual void	version(int major, int minor) = 0;
-
 	typedef IModule* (*t_createModule)(const std::string&, const std::string&, const std::map< std::string, std::string >&);
 
       };
@@ -39,31 +37,12 @@ namespace zia
 	void	name(const std::string&);
 	const std::string&	name() const;
 
-	// virtual void	version(int major, int minor) = 0;
-
       protected:
 	std::string	_name;
-	// int		_major;
-	// int		_minor;
 	std::string	_sigInput;
 	std::string	_sigOutput;
 
       };
-
-      // extern "C"
-      // {
-      
-      // 	/* Module must implement this function with this name! */
-      // 	/**
-      // 	   string s1 : Nom d'un signal
-      // 	   string s2 : Nom d'un signal
-	 
-      // 	   map opt : Parametre optionel
-      // 	*/
-
-      // 	IModule*	createModule(const std::string& s1, const std::string& s2, const std::map< std::string, std::string >&opt);
-      
-      // }
 
     }
   }
